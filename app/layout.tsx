e55cad2c56a7497796d7ab2d30b488210539172e
Nathan <nathan.75@outlook.com>
2024-04-24 18:33:03 +0200

    Push New migration good

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Toaster } from 'react-hot-toast';
+import Navbar from './ui/component/navbar';
 export default function RootLayout({
   children,
 }: {
@@ -10,6 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         {children} <Toaster />
+        <Navbar />
       </body>
     </html>
   );
